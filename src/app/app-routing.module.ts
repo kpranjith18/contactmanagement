419fa90e6d327a74f2287cf8ff1d189f96234e96
kpranjith18 <108397611+kpranjith18@users.noreply.github.com>
2022-07-18 13:41:59 -0400

    fi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 const contactModule = () => import('./contact/contact.module').then(x => x.ContactModule);
 const routes: Routes = [
 
-  {
-    path: 'details/:id',
-    component: ContactDetailComponent,
-  },
 
   { path: 'contact', loadChildren: contactModule },
   { path: '', loadChildren: contactModule }
